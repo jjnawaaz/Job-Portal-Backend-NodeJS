9c90b2c6541c81a697054f5135b146b3db115bd5
jjnawaaz <124430303+jjnawaaz@users.noreply.github.com>
2023-05-20 22:01:18 +0530

    Update dbconfigs.js

--- config/dbconfigs.js
+++ config/dbconfigs.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         const connect = await mongoose.connect(process.env.MONGO_URL)
         console.log(`Connected to MongoDB Database ${mongoose.connection.host}`.bgGreen.white)
     } catch (err) {
-        console.log(`MongoDB Error ${error}`.bgRed.white)
+        console.log(`MongoDB Error ${err}`.bgRed.white)
     }
 }
 
